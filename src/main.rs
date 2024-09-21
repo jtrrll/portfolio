f25b1f1eb5404148ac72587051008e8f31f311f7
Jackson Terrill <jacksonterrill3@gmail.com>
2024-09-21 00:34:40 -0400

    build: set up postgres service

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,20 @@ struct Args {
 enum Command {
     /// Generates a resume as a PDF.
     GenerateResume {
+        /// The database url to connect to.
+        #[arg(long)]
+        db_url: String,
         /// The path to save the resume.
-        #[arg(short, long)]
+        #[arg(long)]
         path: std::path::PathBuf,
     },
     /// Runs the web server.
     Run {
+        /// The database url to connect to.
+        #[arg(long)]
+        db_url: String,
         /// The port number on which the server should listen.
-        #[arg(short, long)]
+        #[arg(long)]
         port: u16,
     },
 }
@@ -29,20 +35,23 @@ enum Command {
 fn main() {
     let args = Args::parse();
     match args.command {
-        Command::GenerateResume { path } => match path.exists() {
-            true => {
-                println!("TODO!! path {:?}", path)
-            }
-            false => Args::command()
-                .error(
-                    clap::error::ErrorKind::ValueValidation,
-                    &format!("The specified path '{}' does not exist.", path.display()),
-                )
-                .exit(),
-        },
-        Command::Run { port } => {
-            // TODO
-            println!("port {}", port);
+        Command::GenerateResume { path, .. } => {
+            if !path.try_exists().unwrap_or(false) {
+                Args::command()
+                    .error(
+                        clap::error::ErrorKind::ValueValidation,
+                        &format!("The specified path '{}' does not exist.", path.display()),
+                    )
+                    .exit()
+            };
+            // TODO: Connect to database
+            // TODO: Call the xp service with the db connection, and save the result
+            println!("Done!");
+        }
+        Command::Run { port, .. } => {
+            // TODO: Connect to database
+            println!("Starting web server on port {}...", port);
+            // TODO: Start the web server
         }
     }
 }
