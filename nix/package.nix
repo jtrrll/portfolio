615b02cea50a70178b546787721e054d904e0a43
Jackson Terrill <jacksonterrill3@gmail.com>
2025-02-26 20:12:19 -0500

    ci: ensure templ files are generated before tests

--- nix/package.nix
+++ nix/package.nix
@@ -9,14 +9,17 @@
     pkg = inputs.gomod2nix.legacyPackages.${system}.buildGoApplication {
       inherit version;
       modules = ../go/gomod2nix.toml;
-      nativeBuildInputs = [pkgs.makeWrapper];
+      nativeBuildInputs = [
+        inputs.templ.packages.${system}.templ
+        pkgs.makeWrapper
+      ];
       pname = name;
       postInstall = ''
         wrapProgram $out/bin/server \
         --set OTEL_RESOURCE_ATTRIBUTES "service.name=${name},service.version=${version}"
       '';
       preBuild = ''
-        ${inputs.templ.packages.${system}.templ}/bin/templ generate
+        templ generate -log-level error
       '';
       src = ../go;
     };
